53320227238080bde73bd1b708960bd473bc0748
Adrian Nürnberger <adrian@nuernberger.me>
2021-07-16 08:27:08 +0200

    WIP

--- tests/FixTest.php
+++ tests/FixTest.php
@@ -8,9 +8,12 @@ it('takes a string and return itself')
     ->toBeInstanceOf(StringFixer::class);
 
 it('throws an error for invalid code', function () {
-    return StringFixer::from('<?php ech"Hello World";')->fix();
+    return StringFixer::from('ech"Hello World";')->fix();
 })->throws(InvalidCode::class);
 
 it('returns a formatted string')
-    ->expect(StringFixer::from('<?php echo"Hello World";')->fix())
-    ->toEqual("<?php echo 'Hello World';".PHP_EOL);
+    ->expect(StringFixer::from('namespace PhpCsFixer\Runner; function x() {echo "Hello World";}')->fix())
+    ->appliedFixers()
+    ->toHaveCount(5)
+    ->__toString()
+    ->toEqual('namespace PhpCsFixer\Runner;'.PHP_EOL.PHP_EOL.'function x()'.PHP_EOL.'{'.PHP_EOL."    echo 'Hello World';".PHP_EOL.'}'.PHP_EOL);
